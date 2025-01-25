6413d426a099bd4eec3a1aded824e080f9ccf8b1
LuizArtur29 <luizarmartinsro99@gmail.com>
2025-01-24 23:07:56 -0300

    Add files via upload

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,78 @@
-package PACKAGE_NAME;
 
-public class GamePanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    //Configs da tela
+    final int originalTileSize = 16; // 16x16 titulo
+    final int scale = 3;
+
+    public final int tileSize = originalTileSize * scale; // 48x48 titulo
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    //FPS
+    int FPS = 60;
+
+    TileManager tileM = new TileManager(this);
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    Player player = new Player(this,keyH);
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    public void run() {
+
+        double drawInterval = 1000000000/FPS; // 0.01666 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while(gameThread != null) {
+
+        update();
+
+        repaint();
+
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000;
+
+                if(remainingTime < 0) {
+                    remainingTime = 0;
+                }
+                Thread.sleep((long) remainingTime);
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public void update() {
+
+        player.update();
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        tileM.draw(g2);
+
+        player.draw(g2);
+
+        g2.dispose();
+    }
 }
--- src/Tile.java
+++ src/Tile.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
+import java.awt.image.BufferedImage;
 
 public class Tile {
+
+    public BufferedImage image;
+    public boolean collision = false;
 }
