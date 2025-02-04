d2b9dc7f3a4a157f576f99ef4643fdd3a70af3f4
MateusVirginio <virginio.dev@gmail.com>
2025-02-04 15:21:51 -0300

    Add files via upload

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -9,8 +9,8 @@ public class GamePanel extends JPanel implements Runnable {
     final int scale = 3;
 
     public final int tileSize = originalTileSize * scale; // 48x48 titulo
-    public final int maxScreenCol = 16;
-    public final int maxScreenRow = 12;
+    public final int maxScreenCol = 24;
+    public final int maxScreenRow = 14;
     public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
